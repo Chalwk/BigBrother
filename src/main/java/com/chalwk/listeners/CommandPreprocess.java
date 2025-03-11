6c98241824c221f6eb9218eb9069a8e7db1a6991
Chalwk77 <jericho.crosby227@gmail.com>
2025-03-12 11:22:22 +1300

    Disabled Social Spy logic and removed hidden commands from config.yml

--- src/main/java/com/chalwk/listeners/CommandPreprocess.java
+++ src/main/java/com/chalwk/listeners/CommandPreprocess.java
@@ -31,62 +31,62 @@ public class CommandPreprocess implements Listener {
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         Player sender = event.getPlayer();
         String command = event.getMessage();
-        String[] args = command.split(" ");
-        PlayerData senderData = PlayerDataManager.getData(sender);
+        //String[] args = command.split(" ");
+        //PlayerData senderData = PlayerDataManager.getData(sender);
 
         // Command spy logic
         if (!hiddenCommand(command)) {
             commandSpy(sender, command);
-            return;
+            //return;
         }
 
         // Handle social spy commands:
-        if (args.length > 1) {
-            StringBuilder commandBuilder = new StringBuilder(args[0]);
-            SocialSpyCommand socialSpyCommand = null;
-
-            // Loop through args to handle multi-word commands
-            for (int i = 1; i < args.length; i++) {
-                String currentCommand = commandBuilder.toString();
-
-                socialSpyCommand = Arrays.stream(SocialSpyCommand.values())
-                        .filter(cmd -> cmd.getCommand().equalsIgnoreCase(currentCommand))
-                        .findFirst()
-                        .orElse(null);
-
-                if (socialSpyCommand != null) {
-                    break;
-                }
-
-                // Add the next word and continue building
-                commandBuilder.append(" ").append(args[i]);
-            }
-
-            if (socialSpyCommand != null) {
-                int recipientIndex = socialSpyCommand.getRecipientIndex();
-
-                // Handle commands that require a recipient to be defined
-                if (recipientIndex > 0 && args.length > recipientIndex + 1) {
-                    String playerName = args[recipientIndex];
-                    Player recipient = Bukkit.getPlayerExact(playerName);
-
-                    if (recipient != null) {
-                        String message = String.join(" ", Arrays.copyOfRange(args, recipientIndex + 1, args.length));
-                        senderData.setLastMessagedPlayer(recipient);
-                        socialSpy(sender, recipient, message);
-                        return;
-                    }
-                }
-
-                // Handle commands that do not require a recipient
-                if (recipientIndex == 0) {
-                    Player lastMessagedPlayer = senderData.getLastMessagedPlayer();
-                    if (lastMessagedPlayer != null) {
-                        String message = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
-                        socialSpy(sender, lastMessagedPlayer, message);
-                    }
-                }
-            }
-        }
+//        if (args.length > 1) {
+//            StringBuilder commandBuilder = new StringBuilder(args[0]);
+//            SocialSpyCommand socialSpyCommand = null;
+//
+//            // Loop through args to handle multi-word commands
+//            for (int i = 1; i < args.length; i++) {
+//                String currentCommand = commandBuilder.toString();
+//
+//                socialSpyCommand = Arrays.stream(SocialSpyCommand.values())
+//                        .filter(cmd -> cmd.getCommand().equalsIgnoreCase(currentCommand))
+//                        .findFirst()
+//                        .orElse(null);
+//
+//                if (socialSpyCommand != null) {
+//                    break;
+//                }
+//
+//                // Add the next word and continue building
+//                commandBuilder.append(" ").append(args[i]);
+//            }
+//
+//            if (socialSpyCommand != null) {
+//                int recipientIndex = socialSpyCommand.getRecipientIndex();
+//
+//                // Handle commands that require a recipient to be defined
+//                if (recipientIndex > 0 && args.length > recipientIndex + 1) {
+//                    String playerName = args[recipientIndex];
+//                    Player recipient = Bukkit.getPlayerExact(playerName);
+//
+//                    if (recipient != null) {
+//                        String message = String.join(" ", Arrays.copyOfRange(args, recipientIndex + 1, args.length));
+//                        senderData.setLastMessagedPlayer(recipient);
+//                        socialSpy(sender, recipient, message);
+//                        return;
+//                    }
+//                }
+//
+//                // Handle commands that do not require a recipient
+//                if (recipientIndex == 0) {
+//                    Player lastMessagedPlayer = senderData.getLastMessagedPlayer();
+//                    if (lastMessagedPlayer != null) {
+//                        String message = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+//                        socialSpy(sender, lastMessagedPlayer, message);
+//                    }
+//                }
+//            }
+//        }
     }
 }
